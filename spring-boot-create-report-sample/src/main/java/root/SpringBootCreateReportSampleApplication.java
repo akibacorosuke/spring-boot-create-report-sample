1b36eff81628c482d4f043f40873ec674123453a
Daichi Akiba <daichiakiba@localhost>
2021-07-25 19:15:57 +0900

    first commit

---
+++ spring-boot-create-report-sample/src/main/java/root/SpringBootCreateReportSampleApplication.java
@@ -0,0 +1,13 @@
+package root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCreateReportSampleApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(SpringBootCreateReportSampleApplication.class, args);
+	}
+
+}
